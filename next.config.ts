7b8188bd7d38d5f75e0ea9f064f1eb35b01b6f7c
RodionTmnkv <rdntmnkv@mail.ru>
2026-07-26 05:36:11 +0300

    chore: add gh-pages deploy config

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,11 @@ import type { NextConfig } from 'next';
 import path from 'path';
 
 const nextConfig: NextConfig = {
+    output: 'export',
+    basePath: process.env.NODE_ENV === 'production' ? '/pet-project' : '',
+    images: {
+        unoptimized: true,
+    },
     sassOptions: {
         includePaths: [path.join(process.cwd(), 'src', 'styles')],
     },
